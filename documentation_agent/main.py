70dd89bb4a3b471b01499556834bb48f4047d4ce
Yuji Egami <yujiegami@MacBook-Air-M3-Yuji.local>
2025-05-22 22:20:57 +0900

    update

--- documentation_agent/main.py
+++ documentation_agent/main.py
@@ -4,7 +4,7 @@ from typing import Annotated, Any, Optional
 from dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.pydantic_v1 import BaseModel, Field
+#from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_openai import ChatOpenAI
 from langgraph.graph import END, StateGraph
 from pydantic import BaseModel, Field
